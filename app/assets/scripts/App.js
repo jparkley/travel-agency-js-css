0ab35c65ab2a063c4d27cacaec5224b195db648d
Jjin SoL <parkleydondon@yahoo.com>
2020-11-28 11:20:47 -0800

    Completed the Reveal-on-Scroll feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,14 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+
+// let revealOnScroll = new RevealOnScroll();
+/* modulizaing */
+new RevealOnScroll(document.querySelectorAll(".package-item"), 90); 
+new RevealOnScroll(document.querySelectorAll(".testimonial-item"), 85);
+let mobileMenu = new MobileMenu();
+
 
 if (module.hot) {
     module.hot.accept();
 }
-
-let mobileMenu = new MobileMenu();
-
-
-
