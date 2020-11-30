02ccaf500553a3e2b50eb6ee111afe6eedd98850
Jjin SoL <parkleydondon@yahoo.com>
2020-11-29 22:15:55 -0800

    Completed build process configuration

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,7 +5,7 @@ import RevealOnScroll from './modules/RevealOnScroll';
 import StickyHeader from './modules/StickyHeader';
 
 // let stickyHeader = new StickyHeader();
-
+//alert("2:00pm");
 new RevealOnScroll(document.querySelectorAll(".package-item"), 90); 
 new RevealOnScroll(document.querySelectorAll(".testimonial-item"), 85);
 
